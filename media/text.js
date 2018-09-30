709573855f6f99744eee8bb7c09e8eb68e29631b
Ladislav Vrbsky <vrbsky@users.noreply.github.com>
2018-09-30 17:45:55 -0300

    Added about section, publication "project" in portfolio

--- media/text.js
+++ media/text.js
@@ -19,7 +19,11 @@
       document.getElementById('headlineName').innerHTML = 'Ladislav Vrbsky';
       document.getElementById('headlinePostName').innerHTML = 'and';
       document.getElementById('headline').innerHTML = 'I <i class="fa fa-heart"></i> AI';
-
+      
+      document.getElementById('aboutHeading').innerHTML = "About";
+      document.getElementById('about-par1').innerHTML = 'I am a Software Developer with passion for Artificial Intelligence<br/>and innovations through software in general. I cover most of my technical bio in my resume (below), as well as on my LinkedIn profile.';
+      document.getElementById('about-par2').innerHTML = "Let's get right into some examples of my work. Check out my portfolio and resume below, <br>as well as the links in the contact section.";
+      
       document.getElementById('contactHeading').innerHTML = 'Get In Touch!';
       document.getElementById('contactDescr').innerHTML = 'Interested to know more about me and my work? ... collaborate? ... hire?<br/>You can reach me through the following means.';
       document.getElementById('aEmail').innerHTML = 'ladislav.vrbsky@gmail.com';
@@ -31,9 +35,9 @@
       // document.getElementById('aGithubIO').href = 'https://vrbsky.github.io';
       document.getElementById('aGithubRepo').innerHTML = 'github.com/vrbsky';
       document.getElementById('aGithubRepo').href = 'https://www.github.com/vrbsky';
-      document.getElementById('aResume').innerHTML = 'Resume';
-      document.getElementById('aResume').href = 'media/pdf/Vrbsky_Resume.pdf';
-      document.getElementById('pSubHeadline').innerHTML = 'I am a Software Developer with passion for Artificial Intelligence<br/>and innovations through software in general.';
+      // document.getElementById('aResume').innerHTML = 'Resume';
+      // document.getElementById('aResume').href = 'media/pdf/Vrbsky_Resume.pdf';
+      // document.getElementById('pSubHeadline').innerHTML = 'I am a Software Developer with passion for Artificial Intelligence<br/>and innovations through software in general.';
       document.getElementById('aHeadlineContinue').innerHTML = 'Continue';
       // document.getElementById('pSubHeadline').innerHTML = 'I am a Software Developer with passion for Artificial Intelligence and software innovations in general!';
       // document.getElementById('pSubHeadline').innerHTML = 'I am a Software Developer with passion for Artificial Intelligence and software innovations in general!';
@@ -42,8 +46,17 @@
       document.getElementById('aDownloadResume').innerHTML = 'Download';
       document.getElementById('aDownloadResume').href = 'media/pdf/Vrbsky_Resume.pdf';
 
+      document.getElementById('project1category').innerHTML = 'Publications';
+      document.getElementById('project1name').innerHTML = 'See my published work, including BS and MS thesis';
+      document.getElementById('aProject1').href = 'https://scholar.google.com.br/citations?hl=en&user=vyvNl6YAAAAJ';
+      document.getElementById('imgProject1').src = 'media/img/portfolio/thumbnails/publications.png';
+//      document.getElementById('imgProject1').style = 'padding: 13% 33%'; // 650 x 350 vs 128 x 128
+     // document.getElementById('imgProject1').style = 'display: block; margin-left: auto; margin-right: auto; margin-top: 5%; marign-bottom: 5%;';// width: 50%;';
+      
       document.getElementById('project2category').innerHTML = 'TensorFlow.js';
       document.getElementById('project2name').innerHTML = 'Simple Linear Regression model purely in your browser';
+      document.getElementById('aProject2').href = 'TF.js-LinearRegression';
+      document.getElementById('imgProject2').src = 'media/img/portfolio/thumbnails/linearRegression.png';
 
       // document.getElementById('').innerHTML = '';
 
